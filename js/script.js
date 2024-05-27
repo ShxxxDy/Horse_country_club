361f077723239e07973dbd94138920fe8958726f
elio rossi <147716496+ShxxxDy@users.noreply.github.com>
2024-05-27 13:42:16 +0200

    aggiunta hamburger menu

--- js/script.js
+++ js/script.js
@@ -39,3 +39,31 @@ immagine_precedente.addEventListener('click', previous);
 
 // Imposta un intervallo per chiamare la funzione "next" ogni 5 secondi
 setInterval(next, 5000); // ogni 5 secondi passa alla immagine successiva
+
+
+// BURGER MENU
+
+// Seleziona l'elemento con la classe "hamburger" e lo assegna alla variabile hamburger
+var hamburger = document.querySelector(".hamburger");
+
+// Seleziona l'elemento con la classe "nav-menu" e lo assegna alla variabile navMenu
+var navMenu = document.querySelector(".nav-menu");
+
+// Seleziona tutti gli elementi <a> all'interno dell'elemento con la classe "nav-menu" e li assegna alla variabile menuItems
+var menuItems = document.querySelectorAll(".nav-menu a");
+
+// Aggiunge un evento di clic all'elemento hamburger
+hamburger.onclick = function() {
+    // Alterna la classe "active" sull'elemento navMenu
+    // Se navMenu ha già la classe "active", la rimuove; altrimenti, la aggiunge
+    navMenu.classList.toggle("active");
+};
+
+// Itera attraverso tutti gli elementi in menuItems (tutti i link nel menu di navigazione)
+menuItems.forEach(function(item) {
+    // Aggiunge un evento di clic a ciascun elemento del menu
+    item.onclick = function() {
+        // Rimuove la classe "active" dall'elemento navMenu
+        navMenu.classList.remove("active");
+    };
+});
